2e6089fb4394cdc6f6a742d9f6076a5ef6c4994b
Henry Earnest <henry_earnest@brown.edu>
2025-10-23 18:27:01 -0400

    optimize noise visualizer by 64x

--- autoload/noise_visualizer.gd
+++ autoload/noise_visualizer.gd
@@ -17,24 +17,19 @@ var tunnel_viz_on := false
 var rarity_viz_on := false
 var quantity_viz_on := false
 
-var noise_type_to_rects: Dictionary[NoiseType, Array] = { # of ColorRects
-	NoiseType.TERRAIN : [],
-	NoiseType.TUNNEL : [],
-	NoiseType.RARITY : [],
-	NoiseType.QUANTITY : [],
-} 
+var rects: Array[ColorRect]
 
 const NOISE_TYPE_TO_COLOR: Dictionary[NoiseType, Color] = {
 	NoiseType.TERRAIN : Color.WHITE,
 	NoiseType.TUNNEL : Color.BLUE,
 	NoiseType.RARITY : Color.GREEN_YELLOW,
-	NoiseType.QUANTITY : Color.ORANGE,
+	NoiseType.QUANTITY : Color.AQUAMARINE,
 }
 const NOISE_TYPE_TO_BOTTOM_COLOR: Dictionary[NoiseType, Color] = {
 	NoiseType.TERRAIN : Color.BLACK,
-	NoiseType.TUNNEL : Color.DARK_SLATE_BLUE,
+	NoiseType.TUNNEL : Color.BLACK,
 	NoiseType.RARITY : Color.DARK_RED,
-	NoiseType.QUANTITY : Color.AQUAMARINE,
+	NoiseType.QUANTITY : Color.BLACK,
 }
 
 var _room: Room
@@ -55,10 +50,14 @@ func _input(event: InputEvent) -> void:
 			toggle_type(NoiseType.QUANTITY)
 
 func clear_rects():
-	for type: NoiseType in noise_type_to_rects.keys():
-		for rect: ColorRect in noise_type_to_rects[type]:
-			rect.queue_free()
-		noise_type_to_rects[type].clear()
+	print("Freeing ", rects.size(), " rects...")
+	for rect: ColorRect in rects:
+		if rect:
+			rect.free.call_deferred()
+		#print("freed")
+	print("Done freeing rects")
+	rects.clear()
+	print("Done clearing rects")
 
 func regenerate_rects_for_room(room: Room):
 	print("REGENERATE CALLED FOR NOISE VISUALIZATION")
@@ -68,32 +67,19 @@ func regenerate_rects_for_room(room: Room):
 	rarity_viz_on = false
 	quantity_viz_on = false
 	clear_rects()
-	for x in range(room._x_bounds.x, room._x_bounds.y + 1):
-		for y in range(room._y_bounds.x, room._y_bounds.y + 1):
-			for type: NoiseType in noise_type_to_rects.keys():
-				noise_type_to_rects[type].append(create_rect(type, Vector2i(x,y)))
-				
-const TILE_SIZE = Vector2(8,8)
-func create_rect(type: NoiseType, tile_pos: Vector2i):
+	for x in range(room._x_bounds.x, room._x_bounds.y + 1, TILE_SCALE):
+		for y in range(room._y_bounds.x, room._y_bounds.y + 1, TILE_SCALE):
+			rects.append(create_rect(Vector2i(x,y)))
+	print("Done regenerating")
+	
+const TILE_SIZE = Vector2(8,8) * 4
+const TILE_SCALE = 4
+func create_rect(tile_pos: Vector2i):
 	var world_pos = _room.wall_layer.map_to_local(tile_pos) - TILE_SIZE / 2
 	var rect: ColorRect = ColorRect.new()
 	rect.position = world_pos
 	rect.size = TILE_SIZE
-	var noise_val: float
-	match type:
-		NoiseType.TERRAIN:
-			noise_val = inverse_lerp(-1, 1, _room.sample_terrain_noise(tile_pos.x, tile_pos.y))
-		NoiseType.TUNNEL:
-			noise_val = inverse_lerp(-1, 1, _room.sample_tunnel_noise(tile_pos.x, tile_pos.y))
-		NoiseType.RARITY:
-			noise_val = _room.sample_rarity_noise(tile_pos.x, tile_pos.y)
-		NoiseType.QUANTITY:
-			noise_val = _room.sample_quantity_noise(tile_pos.x, tile_pos.y)
-		
-	var target_color: Color = lerp(NOISE_TYPE_TO_BOTTOM_COLOR[type],
-	 NOISE_TYPE_TO_COLOR[type], noise_val)
-	target_color.a = 0.0
-	rect.color = target_color
+	rect.color = Color.TRANSPARENT
 	Global.current_floor.add_child(rect)
 	return rect
 
@@ -105,15 +91,42 @@ func toggle_type(type: NoiseType):
 		NoiseType.TERRAIN:
 			target_alpha = 0.0 if terrain_viz_on else 0.5
 			terrain_viz_on = not terrain_viz_on
+			tunnel_viz_on = false
+			rarity_viz_on = false
+			quantity_viz_on = false
 		NoiseType.TUNNEL:
 			target_alpha = 0.0 if tunnel_viz_on else 0.5
 			tunnel_viz_on = not tunnel_viz_on
+			terrain_viz_on = false
+			rarity_viz_on = false
+			quantity_viz_on = false
 		NoiseType.RARITY:
 			target_alpha = 0.0 if rarity_viz_on else 0.5
 			rarity_viz_on = not rarity_viz_on
+			terrain_viz_on = false
+			tunnel_viz_on = false
+			quantity_viz_on = false
 		NoiseType.QUANTITY:
 			target_alpha = 0.0 if quantity_viz_on else 0.5
 			quantity_viz_on = not quantity_viz_on
-	
-	for rect: ColorRect in noise_type_to_rects[type]:
-		rect.color.a = target_alpha
+			terrain_viz_on = false
+			tunnel_viz_on = false
+			rarity_viz_on = false
+	var tween = create_tween().set_parallel()
+	for rect in rects:
+		var noise_val: float
+		var tile_pos: Vector2i = _room.wall_layer.local_to_map(rect.position)
+		match type:
+			NoiseType.TERRAIN:
+				noise_val = inverse_lerp(-1, 1, _room.sample_terrain_noise(tile_pos.x, tile_pos.y))
+			NoiseType.TUNNEL:
+				noise_val = inverse_lerp(-1, 1, _room.sample_tunnel_noise(tile_pos.x, tile_pos.y))
+			NoiseType.RARITY:
+				noise_val = _room.sample_rarity_noise(tile_pos.x, tile_pos.y)
+			NoiseType.QUANTITY:
+				noise_val = _room.sample_quantity_noise(tile_pos.x, tile_pos.y)
+			
+		var target_color: Color = lerp(NOISE_TYPE_TO_BOTTOM_COLOR[type],
+		 NOISE_TYPE_TO_COLOR[type], noise_val)
+		target_color.a = target_alpha
+		tween.tween_property(rect, "color", target_color, 0.4).set_trans(Tween.TRANS_CUBIC)
