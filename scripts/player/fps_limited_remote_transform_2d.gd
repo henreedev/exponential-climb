e82f9b6be9cb83f7f0c1bafe01fe3dc93c057300
Henry Earnest <henry@neptuneflood.com>
2025-11-28 13:59:19 -0500

    RT works in-editor

--- scripts/player/fps_limited_remote_transform_2d.gd
+++ scripts/player/fps_limited_remote_transform_2d.gd
@@ -1,24 +1,42 @@
+@tool
 extends Node2D
-
 class_name FpsLimitedRemoteTransform2D
 
-@export var remote_path: NodePath
+@export var remote_path: NodePath:
+	set(value):
+		remote_path = value
+		_update_node()
+
 @export_range(1, 120) var FPS := 12 
 @export var update_position := true
 @export var update_rotation := true
 @export var update_scale := false
 
+var node: Node2D
 var _frame_timer := 0.0
-@onready var _frame_interval := 1.0 / float(FPS)
-@onready var node: Node2D = get_node(remote_path) 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+var _frame_interval := 1.0
+
+func _ready():
+	_frame_interval = 1.0 / float(FPS)
+	_update_node()
+
+func _process(delta):
+	if not node:
+		return
+
+	_frame_timer -= delta
 	if _frame_timer <= 0.0:
 		_frame_timer = _frame_interval
-		if update_position:
-			node.global_position = global_position
-		if update_rotation:
-			node.global_rotation = global_rotation
-		if update_scale:
-			node.global_scale = global_scale
-	_frame_timer -= delta
+		_update_transform()
+
+func _update_node():
+	if has_node(remote_path):
+		node = get_node(remote_path)
+
+func _update_transform():
+	if update_position and node:
+		node.global_position = global_position
+	if update_rotation:
+		node.global_rotation = global_rotation
+	if update_scale:
+		node.global_scale = global_scale
